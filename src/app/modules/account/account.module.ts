f81f66c2f31101f4bc09e81c6d32f1d1a0164988
novelkhan <novel4004@gmail.com>
2024-05-12 00:56:39 +0600

    confirm-email component is created inside account module in it's dedicated components folder using 'ng g c confirm-email --skip-tests' command

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -5,13 +5,15 @@ import { AccountRoutingModule } from './account-routing.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { SendEmailComponent } from './components/send-email/send-email.component';
+import { ConfirmEmailComponent } from './components/confirm-email/confirm-email.component';
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
     LoginComponent,
-    SendEmailComponent
+    SendEmailComponent,
+    ConfirmEmailComponent
   ],
   imports: [
     CommonModule,
