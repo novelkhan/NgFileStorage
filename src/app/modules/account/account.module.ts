db238051c18646a5cae74ff9b7c4b848b16135dc
novelkhan <novel4004@gmail.com>
2024-05-12 00:53:41 +0600

    send-email component is created inside account module in it's dedicated components folder using 'ng g c send-email --skip-tests' command

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { SendEmailComponent } from './components/send-email/send-email.component';
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    SendEmailComponent
   ],
   imports: [
     CommonModule,
