4254ac37ff2264d62730252ba89a0ebd082abae2
novelkhan <novel4004@gmail.com>
2024-05-12 00:48:55 +0600

    Login component is created inside account module in it's dedicated components folder using 'ng g c login --skip-tests' command

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
