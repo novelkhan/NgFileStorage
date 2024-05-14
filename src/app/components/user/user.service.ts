89b3b599c6829c5a847406e59f9a7ea27e1261e1
novelkhan <novel4004@gmail.com>
2024-05-15 00:01:23 +0600

    User service codes are added

--- src/app/components/user/user.service.ts
+++ src/app/components/user/user.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPlayers(){
+    return this.http.get(`${environment.appUrl}/api/user/get-users`);
+  }
 }
