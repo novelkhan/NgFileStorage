18f4e07838bca5dbc9c62f320b7422c716f060f6
novelkhan <novel4004@gmail.com>
2024-05-17 04:51:29 +0600

    Notification component codes are added

--- src/app/modules/shared/components/modals/notification/notification.component.ts
+++ src/app/modules/shared/components/modals/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-notification',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class NotificationComponent {
 
+  
+  isSuccess: boolean = true;
+  title: string = '';
+  message: string = '';
+
+  constructor (public bsModalRef: BsModalRef) {}
 }
